40e07468a02f9ed3392257661f9e8d7b24a8cb5d
Josh Cotton <jcotton42@outlook.com>
2020-10-17 21:11:29 -0400

    Moved some things arounod

--- ficdl/ffn.py
+++ ficdl/ffn.py
@@ -1,12 +1,9 @@
 from bs4 import BeautifulSoup
-from gzip import GzipFile
-from io import BytesIO
 from random import uniform
 from time import sleep
 from typing import Iterator, List, Optional
-from urllib.request import urlopen
 
-from .types import StoryData
+from .utils import download_and_decompress, StoryData
 
 import logging
 import sys
@@ -75,22 +72,3 @@ def download_story(url: str) -> StoryData:
             chapter_text.append(extract_text(chapter))
     
     return StoryData(title, author, cover_url, chapter_names, chapter_text)
-    
-def download_and_decompress(url):
-    with urlopen(url) as response:
-        if not 200 <= response.status < 300:
-            logger.critical('Failed to download story asset with HTTP status %d', response.status)
-            sys.exit(2) # FIXME, status constant
-        
-        data = response.read()
-        is_gzip = response.headers['Content-Encoding'] == 'gzip'
-    
-    return decompress(data, is_gzip)
-
-def decompress(data, is_gzip):
-    if is_gzip:
-        buf = BytesIO(data)
-        with GzipFile(fileobj=buf) as f:
-            return f.read()
-    else:
-        return data
---
+++ ficdl/utils.py
@@ -0,0 +1,42 @@
+from gzip import GzipFile
+from io import BytesIO
+from typing import Iterable, Iterator
+from urllib.request import urlopen
+
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+class StoryData:
+    title: str
+    author: str
+    cover_url: str
+    chapter_names: Iterable[str]
+    chapter_text: Iterable[Iterator]
+
+    def __init__(self, title, author, cover_url, chapter_names, chapter_text):
+        self.title = title
+        self.author = author
+        self.cover_url = cover_url
+        self.chapter_names = chapter_names
+        self.chapter_text = chapter_text
+
+def download_and_decompress(url):
+    with urlopen(url) as response:
+        if not 200 <= response.status < 300:
+            logger.critical('Failed to download story asset with HTTP status %d', response.status)
+            sys.exit(2) # FIXME, should raise instead
+        
+        data = response.read()
+        is_gzip = response.headers['Content-Encoding'] == 'gzip'
+    
+    return decompress(data, is_gzip)
+
+def decompress(data, is_gzip):
+    if is_gzip:
+        buf = BytesIO(data)
+        with GzipFile(fileobj=buf) as f:
+            return f.read()
+    else:
+        return data
