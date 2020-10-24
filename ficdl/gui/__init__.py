1822534188c047872eed50076e22daa55813f8f0
Josh Cotton <jcotton42@outlook.com>
2020-10-24 18:48:38 -0400

    Close #18

--- ficdl/gui/__init__.py
+++ ficdl/gui/__init__.py
@@ -1,15 +1,32 @@
 import tkinter as tk
+from tkinter import messagebox
+import tkinter.messagebox as messagebox
 import tkinter.ttk as ttk
 
 from .converter import Converter
 from .downloader import Downloader
 from .subscription_manager import SubscriptionManager
+from ficdl import __version__
 
 class Gui(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('ficdl')
+        self.create_menu()
         self.create_widgets()
+
+    def create_menu(self):
+        menubar = tk.Menu(self, tearoff=False)
+
+        file_menu = tk.Menu(menubar, tearoff=False)
+        file_menu.add_command(label='Exit', command=self.quit)
+        menubar.add_cascade(label='File', menu=file_menu)
+
+        help_menu = tk.Menu(menubar, tearoff=False)
+        help_menu.add_command(label='About', command=self.show_about)
+        menubar.add_cascade(label='Help', menu=help_menu)
+
+        self.config(menu=menubar)
 
     def create_widgets(self):
         row = 0
@@ -23,6 +40,9 @@ class Gui(tk.Tk):
 
         row += 1
         SubscriptionManager(self, self).grid(row=row, column=0, sticky=tk.W)
+
+    def show_about(self):
+        messagebox.showinfo('About ficdl', f'ficdl version {__version__}\nMade by jcotton42')
 
 def gui_main():
     Gui().mainloop()
